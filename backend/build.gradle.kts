5b9b8521a3b4969c480c39b7aea85cc56bde51e3
linux_china <libing.chen@gmail.com>
2019-08-23 11:06:35 -0700

    code format

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,39 +4,40 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
     id("org.springframework.boot") version "2.2.0.M5"
-	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
 
 repositories {
     mavenCentral()
-	maven("https://repo.spring.io/milestone")
+    maven("https://repo.spring.io/milestone")
 }
 
 dependencies {
-	val coroutinesVersion = "1.3.0"
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutinesVersion")
-	implementation(project(":shared"))
+    val coroutinesVersion = "1.3.0"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutinesVersion")
+    implementation(project(":shared"))
 }
 
 tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = "1.8"
-            freeCompilerArgs = listOf("-Xjsr305=strict","-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi")
+            freeCompilerArgs =
+                listOf("-Xjsr305=strict", "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi")
         }
     }
-	processResources {
-		dependsOn(":frontend:browserWebpack")
-		from(project(":frontend").projectDir.resolve("src/main/resources")) {
-			into("static")
-		}
-		from(project(":frontend").buildDir.resolve("distributions/frontend-1.0.0-SNAPSHOT.js"))  {
-			into("static")
-		}
-	}
+    processResources {
+        dependsOn(":frontend:browserWebpack")
+        from(project(":frontend").projectDir.resolve("src/main/resources")) {
+            into("static")
+        }
+        from(project(":frontend").buildDir.resolve("distributions/frontend-1.0.0-SNAPSHOT.js")) {
+            into("static")
+        }
+    }
 }
