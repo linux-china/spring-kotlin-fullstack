ab123a4b084886ea3ec83da4a108f88a45d399b9
linux_china <libing.chen@gmail.com>
2019-11-18 16:41:03 +0800

    Update to spring boot 2.2.1

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
-    id("org.springframework.boot") version "2.2.0.RC1"
+    id("org.springframework.boot") version "2.2.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
 
